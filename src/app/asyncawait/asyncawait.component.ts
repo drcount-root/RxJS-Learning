3e16562b2b7ee531d0a93b3e4e62216b34c64307
drcount-root <about.subhamsahu@gmail.com>
2023-05-22 18:08:01 +0530

    async-await

--- src/app/asyncawait/asyncawait.component.ts
+++ src/app/asyncawait/asyncawait.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-asyncawait',
   templateUrl: './asyncawait.component.html',
-  styleUrls: ['./asyncawait.component.scss']
+  styleUrls: ['./asyncawait.component.scss'],
 })
-export class AsyncawaitComponent {
+export class AsyncawaitComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Data Received!')
+      }, 3000)
+    })
+
+    async function getData() {
+      let response = await promise;
+      console.log(response)
+    }
+
+    getData()
+  }
 }
--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-promise',
   templateUrl: './promise.component.html',
-  styleUrls: ['./promise.component.scss']
+  styleUrls: ['./promise.component.scss'],
 })
-export class PromiseComponent {
+export class PromiseComponent implements OnInit {
+  constructor() {}
 
+  dell = { brand: 'Dell', hardDisk: '2 TB', color: 'Black' };
+
+  hp = { brand: 'HP', hardDisk: '1 TB', color: 'Grey' };
+
+  notAvil = { brand: 'Not Available', status: 'Failed' };
+
+  promiseVal: any;
+
+  dellAvailable() {
+    return true;
+  }
+
+  hpAvailable() {
+    return false;
+  }
+
+  ngOnInit(): void {
+    let buyLaptop = new Promise((resolve, reject) => {
+      if (this.dellAvailable()) {
+        return setTimeout(() => resolve(this.dell), 2000);
+      } else if (this.hpAvailable()) {
+        return setTimeout(() => resolve(this.hp), 2000);
+      } else {
+        return setTimeout(() => reject(this.notAvil), 2000);
+      }
+    });
+
+    buyLaptop
+      .then((res) => {
+        this.promiseVal = JSON.stringify(res);
+        console.log(res);
+      })
+      .catch((res) => {
+        this.promiseVal = JSON.stringify(res.brand);
+        console.log(res);
+      });
+  }
 }
